595be381be54087f736491b2cd940740a5b1b121
Estebantrusant <estebita.trujillo@gmail.com>
2025-10-24 12:20:21 +0100

    Class Main edited

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Esteban", LocalDate.of(2004, 6, 25));
+        System.out.println(person);
+    }
 }
