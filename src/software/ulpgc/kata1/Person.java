94b13870773ec6572076d6792cba37d33a6b41f9
Estebantrusant <estebita.trujillo@gmail.com>
2025-10-24 12:18:49 +0100

    Class Person edited

--- src/software/ulpgc/kata1/Person.java
+++ src/software/ulpgc/kata1/Person.java
@@ -1,4 +1,37 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+    private final LocalDate birthDate;
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public int age(){
+        return toYear(LocalDate.now().toEpochDay() - birthDate.toEpochDay());
+    }
+
+    private int toYear(long days) {
+        return (int) (days / 365.25);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
